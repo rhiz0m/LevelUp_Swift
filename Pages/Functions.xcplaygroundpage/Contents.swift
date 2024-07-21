37483aca110cf768146e8550deecce2a67c44305
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-07-21 11:22:39 +0200

    Functions Arcade Challanges

--- Pages/Functions.xcplaygroundpage/Contents.swift
+++ Pages/Functions.xcplaygroundpage/Contents.swift
@@ -149,3 +149,59 @@ func betweenOneAndTen(num: Int) -> Bool {
 }
 
 anyLessThankTen(list: myList, aFunction: betweenOneAndTen(num:))
+
+/*
+  ___             _   _              _                  _
+ | __|  _ _ _  __| |_(_)___ _ _     /_\  _ _ __ __ _ __| |___
+ | _| || | ' \/ _|  _| / _ \ ' \   / _ \| '_/ _/ _` / _` / -_)
+ |_| \_,_|_||_\__|\__|_\___/_||_| /_/ \_\_| \__\__,_\__,_\___|
+
+ */
+
+/*
+🕹 Min
+
+Write a function named min2 that takes two Int values, a and b, and returns the smallest one. Use _ to ignore the external parameter names for both a and b.
+ 
+ > min(1,2)
+ > 1
+*/
+
+
+/*
+🕹 Last Digit
+
+Write a function that takes an Int and returns it’s last digit. Name the function lastDigit. Use _ to ignore the external parameter name.
+
+ > lastDigit(12345)                     Hint: % modulus operator
+ > 5
+ */
+
+
+/*
+🕹 First Numbers
+
+Write a function named first that takes an Int named N and returns an array with the first N numbers starting from 1. Use _ to ignore the external parameter name.
+
+ > first(3)
+ > [1, 2, 3]
+*/
+
+/*
+🕹 Reverse
+
+Write a function named reverse that takes an array of integers named numbers as a parameter. The function should return an array with the numbers from numbers in reverse order.
+
+ > reverse([1, 2, 3])
+ > [3, 2, 1]
+ */
+
+
+/*
+🕹 Sum
+
+Write a function named sum that takes an array of integers and returns their sum. Use a label variable of the word 'of' and a parameter label named numbers.
+
+ > sum(of: [1, 2, 3])
+ > 6
+ */
